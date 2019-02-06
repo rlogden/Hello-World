6a570a114d65c441cd08cf90b9de613bab095109
Rachel Ogden <rlogden@dmacc.edu>
2019-02-05 19:52:34 -0600

    Homework 3 commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.dmacc.codedsm.hw3;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello CodeDSM - Java is Fun!");
+
+    }
+}
